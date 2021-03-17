041642edd8a6612ad515bb56df2443393bd99c5a
Edwin Takahashi <egao@outlook.com>
2021-03-17 16:04:35 -0700

    Create a Poetry project. - include some of the dependencies.
    Refactor the insert_exif.py script.
    - make it more fault-tolerant of missing EXIF tags.
    - ensure the post formatting is kept.

--- scripts/python/insert_exif.py
+++ scripts/python/insert_exif.py
@@ -26,31 +26,64 @@ def find_post(posts_dir, post_name):
     return post.pop()
 
 
+def format_post(contents):
+    if not contents[0] == '---':
+        contents.insert(0, '---')
+    return contents
+
+
 def insert_exif(contents, image_path):
     with exiftool.ExifTool() as et:
-        metadata = et.get_metadata(str(image_path))
+        exif_metadata = et.get_metadata(str(image_path))
 
-    tags = [
-        ('make', 'EXIF:Make'),
-        ('model', 'EXIF:Model'),
-        ('lens', 'EXIF:LensModel'),
-        ('iso', 'EXIF:ISO'),
-        ('capture_date', 'IPTC:DateCreated'),
-    ]
-    for post_tag, metadata_tag in tags:
-        try:
-            data = metadata[metadata_tag]
-        except KeyError:
-            print(f"Key {post_tag} was not found.")
-            continue
+    tags = {
+        'make': [
+            'EXIF:Make'
+        ],
+        'model': [
+            'EXIF:Model'
+        ],
+        'lens': [
+            'EXIF:LensModel'
+        ],
+        'iso': [
+            'EXIF:ISO'
+        ],
+        'capture_date': [
+            'EXIF:DateTimeOriginal',
+            'IPTC:DateCreated'
+        ]
+    }
+
+    insert_index = get_insert_index('---', contents)
+
+    for post_tag, metadata in tags.items():
+        for m in metadata:    
+            try:
+                data = exif_metadata[m]
+            except KeyError:
+                print(f"Key {post_tag} was not found.")
+                continue
+            break
 
         if 'capture_date' in post_tag:
             data = data.replace(':', '-')
+            data = data.split(' ')[0]
 
-        contents.append(': '.join([post_tag, str(data)]))
+        contents.insert(insert_index, ': '.join([post_tag, str(data)]))
 
-    contents.append('---')
+    if get_delimiter_count(contents) < 2:
+        contents.append('---')
+
     return contents
+
+
+def get_insert_index(criteria, contents):
+    return contents[::-1].index(criteria)
+
+
+def get_delimiter_count(contents):
+    return contents.count('---')
 
 
 def load_post_contents(post):
@@ -62,7 +95,7 @@ def write_post_contents(post, contents):
     mdf.rewrite_all_file('\n'.join(contents))
 
 
-def extract_image_path(contents):
+def get_image_path_from_post(contents):
     for item in contents:
         if 'image:' in item:
             image_file = pathlib.Path(item.lstrip('image:').lstrip())
@@ -77,7 +110,7 @@ def extract_image_path(contents):
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument('post_name', nargs='?',
-                        help='Post to insert EXIF for.')
+                        help='Post name to insert EXIF for.')
 
     return parser.parse_args()
 
@@ -87,11 +120,22 @@ def main():
 
     post = find_post(pathlib.Path(repo_root(), '_posts'), args.post_name)
     contents = load_post_contents(post)
-    image_path = extract_image_path(contents)
+    image_path = get_image_path_from_post(contents)
 
-    updated_contents = insert_exif(contents, image_path)
-    write_post_contents(post, updated_contents)
+    contents = insert_exif(contents, image_path)
+    contents = format_post(contents)
+    contents = list(filter(None, contents))
 
+    print(contents)
+    if not contents.count('---') == 2:
+        print('Unexpected number of delimiters in post.')
+        raise SystemExit
+
+    # if contents:
+    #     write_post_contents(post, contents)
+    # else:
+    #     print('Unexpected end of post.')
+    #     raise SystemExit
 
 if __name__ == '__main__':
     main()
